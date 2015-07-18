3c81c42a770acc40a26d8c1ada5bd8369a10009c
ZinChen <zinchen777@gmail.com>
2015-07-18 15:45:44 +0300

    finish step 2 of 5, add fake and factory

--- client/components/input.js
+++ client/components/input.js
@@ -2,7 +2,7 @@ Template.footer.events({
 	'keypress input': function(event) {
 		if (event.charCode == 13) {
 			event.stopPropagation();
-			$(".message-history").append('<div class="message"><a href="" class="message_profile-pic"></a><a href="" class="message_username">scotch</a><span class="message_timestamp">1:31 AM</span><span class="message_star"></span><span class="message_content">' + $('.input-box_text').val() + '</span></div>');
+			Messages.insert({text: $('.input-box_text').val()});
 			$(".input-box_text").val("");
 			return false;
 		}
