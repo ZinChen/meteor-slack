6de9531a0118b79ac5d0b55591a3511fb899abf5
ZinChen <zinchen777@gmail.com>
2015-07-18 15:15:22 +0300

    Finish step 1 of 5, split into templates add fake factory for seeding

---
+++ client/app.js
@@ -0,0 +1,11 @@
+Template.messages.helpers({
+	messages: [
+		{ text: "This is messages" },
+		{ text: "Now from helper" },
+		{ text: "But have a different data context" },
+    { text: "It's why these messages are all different!" },
+    { text: "Hey!" },
+    { text: "What's up man!" },
+    { text: "Hello" }
+	]
+});
